76ae2bc5c70c274e62b08bd0ce4562a881b55b16
dynamicsamic <mirabyansr@gmail.com>
2023-01-26 23:28:55 +0300

    added another view with tests

--- vendors/api.py
+++ vendors/api.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja.pagination import PageNumberPagination, RouterPaginated, paginate
 
@@ -11,6 +12,10 @@ router = RouterPaginated()
 
 
 @router.get("/", response=List[VendorOut], url_name="vendor_list")
-# @paginate(PageNumberPagination)
 def vendor_list(request):
     return Vendor.objects.all()
+
+
+@router.get("/{name}", response=VendorOut, url_name="vendor_detail")
+def vendor_detail(request, name: str):
+    return get_object_or_404(Vendor, name=name)
--- vendors/tests.py
+++ vendors/tests.py
@@ -11,8 +11,9 @@ from tests.utils import (
     get_ninja_view_from_router_paginated,
 )
 
-from .api import router, vendor_list
+from .api import router, vendor_detail, vendor_list
 from .models import Vendor
+from .schemas import VendorOut
 
 VENDOR_NUM = 10
 
@@ -23,11 +24,12 @@ class CreateVendorsMixin:
         super().setUpClass()
         cls.vendors = VendorFactory.create_batch(VENDOR_NUM)
         cls.guest_client = TestClient(router)
-        cls.urls = {"list": "/"}
+        cls.urls = {"list": "/", "detail": "/{name}"}
+        cls.vendor = cls.vendors[0]
 
 
 class VendorsApiTestCase(CreateVendorsMixin, TestCase):
-    # VENDOR LIST SECTION
+    ### VENDOR LIST SECTION ###
     def test_list_uses_right_view(self):
         path = self.urls.get("list")
         view = get_ninja_view_from_router_paginated(router, path)
@@ -50,11 +52,64 @@ class VendorsApiTestCase(CreateVendorsMixin, TestCase):
         resp = self.guest_client.get(f"{path}?page=1")
         items = resp.json().get("items")
         self.assertEqual(len(items), new_page_size)
-        first_five_ids = [i.get("id") for i in items]
-        self.assertEqual(first_five_ids, list(range(1, new_page_size + 1)))
+
+        ids_expected = list(range(1, new_page_size + 1))
+        ids_recieved = [i.get("id") for i in items]
+        self.assertEqual(ids_expected, ids_recieved)
+
+    def test_list_returns_second_page_if_set_in_request(self):
+        new_page_size = 5
+        path = self.urls.get("list")
+        view = get_ninja_view_from_router(router, path)
+        paginator = view.__closure__[1].cell_contents
+        paginator.page_size = new_page_size
+        resp = self.guest_client.get(f"{path}?page=2")
+        items = resp.json().get("items")
+        self.assertEqual(len(items), new_page_size)
+
+        ids_expected = list(range(new_page_size + 1, new_page_size * 2 + 1))
+        ids_recieved = [i.get("id") for i in items]
+        self.assertEqual(ids_expected, ids_recieved)
 
     def test_list_returns_all_vendors(self):
         vendor_num = Vendor.objects.count()
         self.assertEqual(vendor_num, VENDOR_NUM)
         resp = self.guest_client.get(self.urls.get("list"))
         self.assertEqual(resp.json().get("count"), vendor_num)
+
+    def test_list_response_objects_follow_defined_schema(self):
+        expected_keys = VendorOut.schema().get("properties").keys()
+        resp = self.guest_client.get(self.urls.get("list"))
+        for item in resp.json().get("items"):
+            self.assertEqual(item.keys(), expected_keys)
+
+    ### VENDOR DETAIL SECTION ###
+    def test_detail_uses_right_view(self):
+        path = self.urls.get("detail")
+        view = get_ninja_view_from_router(router, path)
+        self.assertEqual(view, vendor_detail)
+
+    def test_detail_with_valid_name_returns_200_status_code(self):
+        path = self.urls.get("detail").format(name=self.vendor.name)
+        resp = self.guest_client.get(path)
+        self.assertEqual(resp.status_code, HTTPStatus.OK)
+
+    def test_detail_with_valid_name_returns_selected_vendor(self):
+        path = self.urls.get("detail").format(name=self.vendor.name)
+        resp = self.guest_client.get(path)
+        attrs_expected = self.vendor.__dict__.copy()
+        attrs_expected.pop("_state")
+        attrs_recieved = resp.json().items()
+        self.assertEqual(attrs_recieved, attrs_expected.items())
+
+    def test_detail_with_invalid_name_returns_404_status_code(self):
+        invalid_name = "invalid-invalid"
+        path = self.urls.get("detail").format(name=invalid_name)
+        resp = self.guest_client.get(path)
+        self.assertEqual(resp.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_detail_response_object_follow_defined_schema(self):
+        expected_keys = VendorOut.schema().get("properties").keys()
+        path = self.urls.get("detail").format(name=self.vendor.name)
+        resp = self.guest_client.get(path)
+        self.assertEqual(resp.json().keys(), expected_keys)
